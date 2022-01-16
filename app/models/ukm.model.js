dfb9b8b384b5ef9c4188f961c8c22a50e174e81c
radityajay <radityajayantara76@gmail.com>
2022-01-16 14:32:28 +0800

    update ukm model

--- app/models/ukm.model.js
+++ app/models/ukm.model.js
@@ -1,13 +1,127 @@
-module.exports = (sequelize, Sequelize) => {
-  const UKM = sequelize.define("ukm", {
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true
-    },
-    name: {
-      type: Sequelize.STRING
-    }
-  });
+const sql = require("./db.js");
 
-  return UKM;
+// constructor
+const Ukm = function(ukm) {
+  this.name = ukm.name;
 };
+
+Ukm.create = (newUkm, result) => {
+  sql.query("INSERT INTO ukms SET ?", newUkm, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("created ukm: ", { id: res.insertId, ...newUkm });
+    result(null, { id: res.insertId, ...newUkm });
+  });
+};
+
+Ukm.findById = (id, result) => {
+  sql.query(`SELECT * FROM ukms WHERE id = ${id}`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("found ukm: ", res[0]);
+      result(null, res[0]);
+      return;
+    }
+
+    // not found Ukm with the id
+    result({ kind: "not_found" }, null);
+  });
+};
+
+Ukm.getAll = (title, result) => {
+  let query = "SELECT * FROM ukms";
+
+  if (title) {
+    query += ` WHERE title LIKE '%${title}%'`;
+  }
+
+  sql.query(query, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log("ukms: ", res);
+    result(null, res);
+  });
+};
+
+Ukm.getAllPublished = result => {
+  sql.query("SELECT * FROM ukms WHERE published=true", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log("ukms: ", res);
+    result(null, res);
+  });
+};
+
+Ukm.updateById = (id, ukm, result) => {
+  sql.query(
+    "UPDATE ukms SET name = ? WHERE id = ?",
+    [ukm.name, id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      if (res.affectedRows == 0) {
+        // not found Ukm with the id
+        result({ kind: "not_found" }, null);
+        return;
+      }
+
+      console.log("updated ukm: ", { id: id, ...ukm });
+      result(null, { id: id, ...ukm });
+    }
+  );
+};
+
+Ukm.remove = (id, result) => {
+  sql.query("DELETE FROM ukms WHERE id = ?", id, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    if (res.affectedRows == 0) {
+      // not found Ukm with the id
+      result({ kind: "not_found" }, null);
+      return;
+    }
+
+    console.log("deleted ukm with id: ", id);
+    result(null, res);
+  });
+};
+
+Ukm.removeAll = result => {
+  sql.query("DELETE FROM ukms", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log(`deleted ${res.affectedRows} ukms`);
+    result(null, res);
+  });
+};
+
+module.exports = Ukm;
