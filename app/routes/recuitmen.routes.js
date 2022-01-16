d7888254851472f642b642bbee82f8f33e7cec2d
radityajay <radityajayantara76@gmail.com>
2022-01-16 14:43:17 +0800

    upload router recuitmen

---
+++ app/routes/recuitmen.routes.js
@@ -0,0 +1,28 @@
+module.exports = app => {
+  const recuitmens = require("../controllers/recuitmen.controller");
+
+  var router = require("express").Router();
+
+  // Create a new Tutorial
+  router.post("/", recuitmens.create);
+
+  // Retrieve all Tutorials
+  router.get("/", recuitmens.findAll);
+
+  // Retrieve all published Tutorials
+  router.get("/join", recuitmens.findAllJoin);
+
+  // Retrieve a single Tutorial with id
+  // router.get("/:id", recuitmens.findOne);
+
+  // // Update a Tutorial with id
+  // router.put("/:id", recuitmens.update);
+
+  // // Delete a Tutorial with id
+  // router.delete("/:id", recuitmens.delete);
+
+  // // Delete all Tutorials
+  // router.delete("/", recuitmens.deleteAll);
+
+  app.use('/api/recuitmens', router);
+};
