7e886754cf98dc0295fc513e677bdf9637a9e608
radityajay <radityajayantara76@gmail.com>
2022-01-16 14:39:49 +0800

    update model prodi

--- app/models/prodi.model.js
+++ app/models/prodi.model.js
@@ -1,13 +1,127 @@
-module.exports = (sequelize, Sequelize) => {
-  const Prodi = sequelize.define("prodi", {
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true
-    },
-    name: {
-      type: Sequelize.STRING
-    }
-  });
+const sql = require("./db.js");
 
-  return Prodi;
+// constructor
+const Prodi = function(prodi) {
+  this.name = prodi.name;
 };
+
+Prodi.create = (newProdi, result) => {
+  sql.query("INSERT INTO prodis SET ?", newProdi, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("created prodi: ", { id: res.insertId, ...newProdi });
+    result(null, { id: res.insertId, ...newProdi });
+  });
+};
+
+Prodi.findById = (id, result) => {
+  sql.query(`SELECT * FROM prodis WHERE id = ${id}`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("found prodi: ", res[0]);
+      result(null, res[0]);
+      return;
+    }
+
+    // not found Prodi with the id
+    result({ kind: "not_found" }, null);
+  });
+};
+
+Prodi.getAll = (title, result) => {
+  let query = "SELECT * FROM prodis";
+
+  if (title) {
+    query += ` WHERE title LIKE '%${title}%'`;
+  }
+
+  sql.query(query, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log("prodis: ", res);
+    result(null, res);
+  });
+};
+
+Prodi.getAllPublished = result => {
+  sql.query("SELECT * FROM prodis WHERE published=true", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log("prodis: ", res);
+    result(null, res);
+  });
+};
+
+Prodi.updateById = (id, prodi, result) => {
+  sql.query(
+    "UPDATE prodis SET title = ?, description = ?, published = ? WHERE id = ?",
+    [prodi.title, prodi.description, prodi.published, id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      if (res.affectedRows == 0) {
+        // not found Prodi with the id
+        result({ kind: "not_found" }, null);
+        return;
+      }
+
+      console.log("updated prodi: ", { id: id, ...prodi });
+      result(null, { id: id, ...prodi });
+    }
+  );
+};
+
+Prodi.remove = (id, result) => {
+  sql.query("DELETE FROM prodis WHERE id = ?", id, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    if (res.affectedRows == 0) {
+      // not found Prodi with the id
+      result({ kind: "not_found" }, null);
+      return;
+    }
+
+    console.log("deleted prodi with id: ", id);
+    result(null, res);
+  });
+};
+
+Prodi.removeAll = result => {
+  sql.query("DELETE FROM prodis", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log(`deleted ${res.affectedRows} prodis`);
+    result(null, res);
+  });
+};
+
+module.exports = Prodi;
